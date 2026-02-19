463d7e61351e3c9816e34dd7bcf82a2caea4c036
neoflame99 <43754377+neoflame99@users.noreply.github.com>
2026-02-19 17:26:22 +0900

    Refactor APB driver for improved request handling

--- APB/src/apb_driver.cpp
+++ APB/src/apb_driver.cpp
@@ -17,11 +17,13 @@ void APBdriver::procreq(){
             }while(!rdat_valid);
             PRdat.Push(rdat);
         }
+        cout <<"preq.addr: " << preq.addr <<", preq.wr_n: " << preq.wr_n << endl;
         wait();
     }
 }
 void APBdriver::apbctrl(){
     req_ready = false;
+    req_rdy_dly = false;
     rdat_valid = false;
     PSEL    = false;
     PWRITE  = false;
@@ -32,29 +34,39 @@ void APBdriver::apbctrl(){
     while(1){
         do{
             wait();
+            rdat_valid = false;
         }while( !req_avail);
+        req_rdy_dly = req_ready;
+        if(preq.addr >= ADR_MIN && preq.addr <= ADR_MAX){
+            PSEL = true;
+        }else {
+            PSEL = false;
+        }
         req_ready = false;
         PSEL   = true;
         PADDR  = preq.addr; 
         PWDATA = preq.wdat;
         PWRITE = preq.wr_n;
         wait();
-        PENABLE= true;
+        if(req_rdy_dly){
+            PENABLE= true;
+        }
         do{
             wait();
         }while(!PREADY);
+        PENABLE = false;
         if(!PWRITE){
             rdat = PRDATA.read().to_uint();
             rdat_vaild = true;
         }
         if(PSLVERR){
-            string msg = string("Addr: ")+to_string(preq.addr);
+            string msg = string("Addr: ")+to_string(PADDR.read().to_uint());
             if(PWRITE){
-                msg += string(", Wdata: ")+to_string(preq.wdat)+": Writing can't be done!";
+                msg += string(", Wdata: ")+to_string(PWDATA.read().to_uint())+": Writing can't be done!";
             }else{
-                msg += string(", Rdata: ")+to_string(PRDATA.read().to_uint())+": Reading can't be done!";
+                msg += ": Reading can't be done!";
             }
-            SC_REPORT_INFO("PSLVERR", msg.c_str());
+            SC_REPORT_WARNING("PSLVERR", msg.c_str());
         }
         req_ready = true;
     }
